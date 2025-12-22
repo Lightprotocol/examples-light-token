6e83b95ecdf29aac85d104587d21391096db9a06
Swenschaeferjohann <swen@lightprotocol.com>
2025-12-22 13:48:22 +0100

    quickstart

--- devnet-quickstart/index.ts
+++ devnet-quickstart/index.ts
@@ -10,18 +10,21 @@ import { homedir } from "os";
 import { readFileSync } from "fs";
 
 // Load wallet
-const keypairPath = `${homedir()}/.config/solana/id.json`;
 const payer = Keypair.fromSecretKey(
-    new Uint8Array(JSON.parse(readFileSync(keypairPath, "utf8")))
+    new Uint8Array(
+        JSON.parse(readFileSync(`${homedir()}/.config/solana/id.json`, "utf8"))
+    )
 );
 
-// You can use any compatible RPC endpoint
-dotenv.config();
-const RPC_URL = `https://devnet.helius-rpc.com?api-key=${process.env.API_KEY!}`;
+// Use a compatible RPC provider, such as Helius and Triton.
+// Enable to use Devnet:
+// dotenv.config();
+// const RPC_URL = `https://devnet.helius-rpc.com?api-key=${process.env.API_KEY!}`;
 
-async function main() {
+(async function () {
     const rpc = createRpc();
 
+    // 1. create mint
     const mintAuthority = payer;
     const { mint, transactionSignature } = await createMintInterface(
         rpc,
@@ -31,7 +34,7 @@ async function main() {
         9
     );
 
-    // Mint compressed tokens to a recipient token account.
+    // 2. get recipient
     const recipient = Keypair.generate();
     const recipientAta = await getOrCreateAtaInterface(
         rpc,
@@ -40,6 +43,7 @@ async function main() {
         recipient
     );
 
+    // 3. mint to recipient
     const txId = await mintToInterface(
         rpc,
         payer,
@@ -64,9 +68,4 @@ async function main() {
             2
         )
     );
-}
-
-main().catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+})();
